3c8f2789aaa4005ffeaeebb5e8db7ea2dad5f8df
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-05-29 11:04:25 +0530

    Added: `discord-moderation-bot/src/commands/userCommands.py`

---
+++ discord-moderation-bot/src/commands/userCommands.py
@@ -0,0 +1,43 @@
+```python
+# userCommands.py
+
+class UserCommands:
+    def __init__(self):
+        pass
+
+    def warn_user(self, user_id):
+        pass
+
+    def mute_user(self, user_id):
+        pass
+
+    def kick_user(self, user_id):
+        pass
+
+    def ban_user(self, user_id):
+        pass
+
+    def set_custom_rules(self, rules):
+        pass
+
+    def adjust_filter_sensitivity(self, sensitivity):
+        pass
+
+    def customize_responses(self, responses):
+        pass
+
+    def manage_user_roles(self, user_id, roles):
+        pass
+
+    def manage_user_permissions(self, user_id, permissions):
+        pass
+
+    def manage_user_restrictions(self, user_id, restrictions):
+        pass
+
+    def track_user_infractions(self, user_id, infractions):
+        pass
+
+    def show_help_command(self):
+        pass
+```  
